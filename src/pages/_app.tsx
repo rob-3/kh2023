2c6f8c18a8efcc9252a835cd8533f024b7dea48d
Rikimbili <git@raciel.dev>
2023-10-07 12:06:46 -0400

    Auto stash before merge of "main" and "origin/main"

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,28 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import { styleReset } from "react95";
+
+import tokyo from "react95/dist/themes/tokyoDark";
+
+const GlobalStyles = createGlobalStyle`
+  ${styleReset}
+`;
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyles />
+      <ThemeProvider theme={tokyo}>
+        <div className={"bg-[url('/windowsHeader16-9.png')]"}>
+          <div className={"bg-zinc-950/50"}>
+            <Component {...pageProps} />
+          </div>
+        </div>
+      </ThemeProvider>
+    </>
+  );
 };
 
 export default api.withTRPC(MyApp);
