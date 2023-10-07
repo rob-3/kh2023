9e0e3e1cd5913ac79a9490203351b302ecffb144
Robert Boyd III <git@robertboyd.dev>
2023-10-07 13:06:00 -0400

    Add code

---
+++ src/pages/api/chat.ts
@@ -0,0 +1,21 @@
+// ./app/api/chat/route.js
+import OpenAI from 'openai';
+import { OpenAIStream, StreamingTextResponse } from 'ai';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+export const runtime = 'edge';
+
+export async function POST(req: NextApiRequest, res: NextApiResponse) {
+  const { messages } = await req.body;
+  const response = await openai.chat.completions.create({
+    model: 'gpt-3.5-turbo',
+    stream: true,
+    messages,
+  });
+  const stream = OpenAIStream(response);
+  return new StreamingTextResponse(stream);
+}
---
+++ src/pages/api/dalle.ts
@@ -0,0 +1,21 @@
+// ./app/api/chat/route.js
+import OpenAI from 'openai';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+export const runtime = 'edge';
+
+export async function POST(req: NextApiRequest, res: NextApiResponse) {
+  const { prompt } = await req.body;
+  const response = await openai.images.generate({
+    prompt,
+    n: 1,
+    size: "1024x1024",
+  })
+  res.json({
+    url: response.data[0]!.url
+  })
+}
