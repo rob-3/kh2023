11e8b59ee404f23b3776a04b7372959e682a70d6
Robert Boyd III <git@robertboyd.dev>
2023-10-08 10:15:18 -0400

    fix the build

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -4,9 +4,11 @@ import { drizzle } from "drizzle-orm/planetscale-serverless";
 import { env } from "~/env.mjs";
 import * as schema from "./schema";
 
+/*
 export const db = drizzle(
   new Client({
     url: env.DATABASE_URL,
   }).connection(),
   { schema }
 );
+*/
