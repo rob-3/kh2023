30a05e64b5ead783f8e8f1699c898938b44affc1
Robert Boyd III <git@robertboyd.dev>
2023-10-07 22:33:25 -0400

    Fix dalle

--- src/pages/api/dalle.ts
+++ src/pages/api/dalle.ts
@@ -1,18 +1,19 @@
 // ./app/api/chat/route.js
 import OpenAI from "openai";
-import { type NextApiResponse } from "next";
-import { type NextRequest } from "next/server";
+import { NextApiRequest, type NextApiResponse } from "next";
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-export default async function handler(req: NextRequest, res: NextApiResponse) {
-  const { prompt } = (await req.json()) as { prompt: string };
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { prompt } = (req.body) as { prompt: string };
   const response = await openai.images.generate({
     prompt,
     n: 1,
     size: "1024x1024",
   });
-  return new Response(response.data[0]!.url);
+  res.status(200).json({
+    imageURL: response.data[0]!.url
+  })
 }
